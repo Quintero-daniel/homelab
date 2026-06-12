1c4b03166c2d76ae517be5c9e8f473c5c1640ba7
Daniel Quintero <daniel.quintero@netapp.com>
2026-06-12 19:49:27 +1000

    Add initial backend: main entrypoint, application.properties and pom

---
+++ backend/src/main/java/com/homelab/HomelabApplication.java
@@ -0,0 +1,12 @@
+package com.homelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomelabApplication.class, args);
+    }
+}
